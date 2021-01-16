579f63edeccdb052c42ae4119ed17f7a6b09e54e
leo-allves <leonardoallves1992@gmail.com>
2021-01-16 02:47:01 -0300

    criando conexao
    Foi feita a criação da conexão e também terminado o insert que está ok.

--- form/_inserir_produto.php
+++ form/_inserir_produto.php
@@ -1,5 +1,5 @@
 <?php
-    //include 'conexao.php'
+    include 'conexao.php';
 
     //variavel   = atribuição['name];
     $nroproduto = $_POST['nroproduto']; //recebe o valor do atributo
@@ -16,4 +16,32 @@
     //criando comando de insersão e atribuindo a variavel $sql
     $sql ="INSERT INTO `estoque`(`nroproduto`, `nomeproduto`, `categoria`, `quantidade`, `fornecedor`) VALUES ($nroproduto,'$nomeproduto','$categoria',$quantidade,'$fornecedor')";
     //e usando aspas simples na sql quando e texto e numero a variavel nao coloca aspas
+
+
+    //inserindo BD = conectando e dando comando de insersão
+    $inserir = mysqli_query($conexao, $sql);
+
+
+
 ?>
+
+    <!--Mensagem de sucesso!-->
+    <link rel = "preconnect" href = "https://fonts.gstatic.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Yusei+Magic&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="../css/bootstrap.css">
+    <link rel="stylesheet" href="../css/style.css">
+
+    <div class="container" style="width: 500px;margin-top: 20px;">
+        <center>
+            <h4>Produto Adicionado com sucesso!</h4>
+        </center>
+        
+        <div style="padding-top: 20px">
+            <center>
+               <a href="index.php" role="button" class="btn btn=sm btn-primary">Cadastrar novo Ítem</a> 
+            </center>
+            
+        </div>
+        
+    </div>
---
+++ form/conexao.php
@@ -0,0 +1,14 @@
+<?php 
+
+    $servername = "localhost";
+    $database = "bd_estoque";
+    $username = "root";
+    $password = "1";
+
+    //CRIANDO A CONEXAO
+    $conexao = mysqli_connect($servername, $username, $password, $database);
+
+
+
+
+?>
