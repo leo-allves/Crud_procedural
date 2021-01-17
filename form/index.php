7692cb8cd5b5141bf44708982b166cf78f5d208a
leo-allves <leonardoallves1992@gmail.com>
2021-01-17 09:14:17 -0300

    atualizando e formando paineis de menu

--- form/index.php
+++ form/index.php
@@ -11,51 +11,27 @@
     <title>Formulário de Cadastro</title>
 </head>
 <body>
-    <div class="container" id="tamanhoContainer">
-        <h4>Formulário de Cadastro</h4>
-        <!--Quando vamos enviar algo ou declarar informações em variaveis usamos o POST
-            Quando vamos editar um produto relacionado a id, mandamos o id na url e então usamos GET para pegar essas informações na url
-            Atribuir um valor POST // Pegar um valor GET-->
-        <form style="margin-top: 20px;" action="_inserir_produto.php" method="post">
-            <div class="form-group">
-                <label>Nº Produto</label>
-                <input type="number" class="form-control" name="nroproduto" placeholder="Insira o número do produto" required autocomplete="off">    
+    <div class="container"  style="margin-top:100px;">
+        <div class="row">
+            <div class="col-sm-6">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">Adicionar Produto</h5>
+                            <p class="card-text">Opção para adicionar produto em nosso estoque.</p>
+                            <a href="adicionar_produto.php" class="btn btn-info">Cadastrar Produto</a>    
+                    </div>
+                </div>
             </div>
-
-            <div class="form-group">
-                <label>Nome Produto</label>
-                <input type="text" class="form-control" name="nomeproduto" placeholder="Insira o nome do produto" required autocomplete="off">     
+            <div class="col-sm-6">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">Lista de Produtos</h5>
+                            <p class="card-text">Opção para pesquisar produtos.</p>
+                            <a href="listar_produtos.php" class="btn btn-info">Pesquisar</a>
+                    </div>
+                </div>
             </div>
-
-            
-
-            <div class="form-group">
-                <label>Categoria</label>
-                <select class="form-control" name="categoria">
-                    <option>Periférico</option>
-                    <option>Hardware</option>
-                    <option>Software</option>
-                    <option>Celulares</option>
-                </select>
-            </div>
-
-            <div class="form-group">
-                <label>Quantidade</label>
-                <input type="number" class="form-control" name="quantidade" placeholder="Insira a quantidade do produto" required autocomplete="off">     
-            </div>
-
-            <div class="form-group">
-                <label>Fornecedor</label> 
-                <select class="form-control" name="fornecedor">
-                    <option>Fornecedor A</option>
-                    <option>Fornecedor B</option>
-                    <option>Fornecedor C</option>
-                </select>
-            </div>
-            <!--usamos o botao submit quando queremos enviar alguma coisa-->
-            <button type="submit" class="btn btn-secondary btn-sm" id="button_submit">Cadastrar</button>
-
-        </form>
+        </div>
     </div>
     
     <script type="bootstrap/js" src="..//js/bootstrap.js"></script>
